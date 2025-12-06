b1ab4667c359a982af0a5a4e669fb4df970cec8c
Chris Rowe <chris@rowe.com>
2025-12-06 12:29:42 -0500

    Set up lighttpd web server with permissions
    Added setup for lighttpd web server and permissions.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+# Web server to test things are working
+sudo apt install lighttpd -y
+sudo chown -R www-data:www-data /var/www/html
+sudo usermod -aG www-data $USER
+sudo chown -R $USER:www-data /var/www/html
+sudo chmod -R 775 /var/www/html
+sudo chmod g+s /var/www/html
+cp ./index.html /var/www/html/
+
 # tmux, in case you loose the connection
 sudo apt install tmux
 tmux
